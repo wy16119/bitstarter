4d9c2f2f64a1e029f8f64693b148438d2df6f511
Ubuntu <wzy@umich.edu>
2013-09-13 21:56:11 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var fs = require('fs');
-var app = express();
-app.use(express.logger());
+var htmlfile = "index.html";
 
-var buffer = fs.readFileSync('index.html');
-var content = buffer.toString('utf-8');
+var app = express(express.logger());
 
-app.get('/', function(request, response) {
-  response.send(content);
+app.get('/', function (request, response) {
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
